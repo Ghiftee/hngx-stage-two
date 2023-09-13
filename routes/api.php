7bfa9ec8e971ed9132bda124421f26956bbd7dc5
Ghiftee <gigibetine@gmail.com>
2023-09-13 08:23:53 +0100

    Feature: Create read api

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/', [PersonController::class, 'create']);
+Route::get('/{id}', [PersonController::class, 'read']);
